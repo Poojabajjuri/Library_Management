af30bd707022c58c57d6869bd7e8748bcae0adc4
Pooja Bajjuri <pooja.bajjuri@proclink.local>
2026-07-29 14:33:05 +0530

    Add SQS logging

--- services/sqs.js
+++ services/sqs.js
@@ -7,12 +7,31 @@ const client = new SQSClient({
 const QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/345366156005/BookQueue";
 
 async function sendBookMessage(message) {
+
+    console.log("========== SENDING TO SQS ==========");
+    console.log(message);
+
     const command = new SendMessageCommand({
         QueueUrl: QUEUE_URL,
         MessageBody: JSON.stringify(message)
     });
 
-    await client.send(command);
+    try {
+
+        const result = await client.send(command);
+
+        console.log("========== SQS SUCCESS ==========");
+        console.log(result);
+
+        return result;
+
+    } catch (err) {
+
+        console.error("========== SQS ERROR ==========");
+        console.error(err);
+
+        throw err;
+    }
 }
 
 module.exports = { sendBookMessage };
